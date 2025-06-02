db444fd536f72ceeb30a7df7068be08ddcab3330
Bima Akar <81931118+c0desk1@users.noreply.github.com>
2025-06-03 01:51:42 +0700

    Update tips.js

--- js/d/tips.js
+++ js/d/tips.js
@@ -3,9 +3,9 @@ function loadPostsTips() {
   if (!container) return;
 
   container.innerHTML = '<p>Loading...</p>';
-  fetch('https://opensheet.elk.sh/1l7d-9BwbbH5I-jj_Uw0-eA2mkJEWV_yWyt9RquhM_XY/draft-tips')
+  fetch('https://opensheet.elk.sh/1l7d-9BwbbH5I-jj_Uw0-eA2mkJEWV_yWyt9RquhM_XY/tips-list')
     .then(function(res) {
-      if (!res.ok) throw new Error('Gagal fetch tips-trik.json');
+      if (!res.ok) throw new Error('Gagal fetch tips dantrik');
       return res.json();
     })
     .then(function(data) {
