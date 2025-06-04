ab762237e498fd24ae3ba98bca42d964f678d4c0
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-04 23:15:04 +0700

    Update stats.js

--- js/u/stats.js
+++ js/u/stats.js
@@ -1,16 +1,18 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDtMdCf91Ihh-SlhoZHLV4Taxg2YPmks14",
-  authDomain: "bima-akbar-web.firebaseapp.com",
-  databaseURL: "https://bima-akbar-web-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bima-akbar-web",
-  storageBucket: "bima-akbar-web.appspot.com",
-  messagingSenderId: "521611265429",
-  appId: "1:521611265429:web:9e6c64385b5abcbad6e29c"
-};
+// Pastikan hanya dideklarasikan sekali di global scope
+if (!window.firebaseConfig) {
+  window.firebaseConfig = {
+    apiKey: "AIzaSyDtMdCf91Ihh-SlhoZHLV4Taxg2YPmks14",
+    authDomain: "bima-akbar-web.firebaseapp.com",
+    databaseURL: "https://bima-akbar-web-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "bima-akbar-web",
+    storageBucket: "bima-akbar-web.appspot.com",
+    messagingSenderId: "521611265429",
+    appId: "1:521611265429:web:9e6c64385b5abcbad6e29c"
+  };
 
-// Inisialisasi Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  if (!firebase.apps.length) {
+    firebase.initializeApp(window.firebaseConfig);
+  }
 }
 
 // Ambil statistik dari Firebase secara real-time
