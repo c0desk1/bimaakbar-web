4938848d4743b9810adea492f4fde4c92e441d4a
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-05 10:32:23 +0700

    Update label.js

--- js/u/label.js
+++ js/u/label.js
@@ -17,20 +17,17 @@ function loadCategoryLabels() {
         fetch(`https://opensheet.elk.sh/10fSdWnRM2rYLYfJufWl-IkBeul2CgZSoUmOaeneO8xk/${label.sheet}`)
             .then(res => res.json())
             .then(data => {
-                const post = data[0]; // Ambil postingan terbaru
+                const post = data[0];
                 const thumb = post.thumbnail || '/assets/error.jpg';
-
                 const card = document.createElement('a');
                 card.href = label.url;
                 card.className = 'category-card';
                 card.style.backgroundImage = `url('${thumb}')`;
-
                 card.innerHTML = `
-          <div class="category-content">
-            <h3>${label.title}</h3>
-          </div>
-        `;
-
+		          <div class="category-content">
+		            <h3>${label.name}</h3>
+		          </div>
+		        `;
                 container.appendChild(card);
             })
             .catch(err => {
@@ -38,5 +35,4 @@ function loadCategoryLabels() {
             });
     });
 }
-
 window.loadCategoryLabels = loadCategoryLabels;
