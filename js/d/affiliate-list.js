59b08f86ac26e1e0c6cb2f64b5893e53ed15cf77
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-07 00:13:57 +0700

    Update affiliate-list.js

--- js/d/affiliate-list.js
+++ js/d/affiliate-list.js
@@ -16,11 +16,8 @@ function renderAffiliateItems() {
                 card.href = item.url;
                 card.target = '_blank';
                 card.className = 'affiliate-card';
-
-                // Tambahan: Pembungkus gambar
+                
                 var imgDiv = document.createElement('div');
-                imgDiv.className = 'affiliate-img';
-
                 var img = document.createElement('img');
                 img.src = item.img;
                 img.alt = item.alt ? item.alt : 'Affiliate Item';
@@ -29,25 +26,9 @@ function renderAffiliateItems() {
                     this.onerror = null;
                     this.src = 'assets/error.jpg';
                 };
-                img.style.width = "100%";
-                img.style.height = "100%";
-                img.style.objectFit = "cover";
-                img.style.display = "block";
-
+ 
                 imgDiv.appendChild(img);
                 card.appendChild(imgDiv);
-
-                // Label affiliate (opsional)
-                if(item.alt) {
-                    var label = document.createElement('div');
-                    label.className = 'affiliate-label';
-                    label.innerText = item.alt;
-                    label.style.textAlign = "center";
-                    label.style.fontSize = "14px";
-                    label.style.marginTop = "8px";
-                    card.appendChild(label);
-                }
-
                 affiliateList.appendChild(card);
             }
         })
