7249799e0a6e5aad5c4395814d73389647189310
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-21 16:08:51 +0700

    Update supabaseConfig.js

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_CONFIG = {
-    URL: "https://wgcslximcxnprmfqtsoh.supabase.co",
+   URL: "https://wgcslximcxnprmfqtsoh.supabase.co",
     ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnY3NseGltY3hucHJtZnF0c29oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTE5NzEsImV4cCI6MjA2NTMyNzk3MX0.K0xWAiVdU9vAwJepHV5rMBROBoBDTpXtczXoQopEZQk"
 };
