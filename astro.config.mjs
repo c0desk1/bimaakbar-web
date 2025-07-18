3e16027cacaf1042dfc8d67ae39157338e5b1723
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-18 18:10:01 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,55 +1,72 @@
-// @ts-nocheck
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
+import typography from '@tailwindcss/typography'; // Impor dengan ES module
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bimaakbar.vercel.app",
+  site: "https:                        
   vite: {
     plugins: [
-		tailwindcss(),
-		require('@tailwindcss/typography'),
-	],
+      tailwindcss(),
+      typography(),                             
+    ],
   },
   integrations: [
     react(),
     sitemap(),
-    mdx(
-      {
-        shikiConfig: {
-          theme: 'dracula',
-          themes: {
-            light: 'github-light',
-            dark: 'github-dark',
-          },
-          defaultColor: false,
-          langs: [],
-          langAlias: {
-            cjs: "javascript"
-          },
-          wrap: false,
-          transformers: [],
+    mdx({
+      shikiConfig: {
+        theme: 'dracula',
+        themes: {
+          light: 'github-light',
+          dark: 'github-dark',
         },
-        syntaxHighlight: {
-          type: 'shiki',
-          excludeLangs: ['mermaid', 'math', 'markdown', 'js'],
+        defaultColor: false,
+        langs: [],
+        langAlias: {
+          cjs: "//bimaakbar.vercel.app",
+  vite: {
+    plugins: [
+      tailwindcss(),
+      typography(), // Gunakan plugin typography
+    ],
+  },
+  integrations: [
+    react(),
+    sitemap(),
+    mdx({
+      shikiConfig: {
+        theme: 'dracula',
+        themes: {
+          light: 'github-light',
+          dark: 'github-dark',
         },
-        rehypePlugins: [
-          rehypeHeadingIds,
-          [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-          [rehypeAccessibleEmojis],
-        ],
-        gfm: true,
-        optimize: true,
+        defaultColor: false,
+        langs: [],
+        langAlias: {
+          cjs: "javascript"
+        },
+        wrap: false,
+        transformers: [],
       },
-    ),
+      syntaxHighlight: {
+        type: 'shiki',
+        excludeLangs: ['mermaid', 'math', 'markdown', 'js'],
+      },
+      rehypePlugins: [
+        rehypeHeadingIds,
+        [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+        [rehypeAccessibleEmojis],
+      ],
+      gfm: true,
+      optimize: true,
+    }),
   ],
   markdown: {
     gfm: true,
