3df0e0624ed6e07a579f4d75aa49ee39b01329e7
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-20 18:50:46 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
     sitemap(), 
     expressiveCode(
       {
-        themes: ['aurora-x', 'catppuccin-latte'],
+        themes: ['aurora-x', 'catppuccino-latte'],
         shiki: {
           langs: [
             'astro', 
@@ -135,7 +135,7 @@ export default defineConfig({
       wrap: true,
       defaultColor: false,
     },
-    syntaxHighlight: 'shiki',
+    syntaxHighlight: false,
     remarkPlugins: [
       remarkGfm,
     ],
