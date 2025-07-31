ab6448d2a5ec59d7b899c48dbf746e22d60b90fd
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-31 14:50:50 +0700

    Update Card.tsx

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { PostMeta } from "@/types"
-import { formatDate } from "@lib/utils"
+import { formatDate } from "@/lib/utils"
 
 export default function Card({
   slug,
