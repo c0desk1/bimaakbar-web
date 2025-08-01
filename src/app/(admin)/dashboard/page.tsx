e354dc5b68b4c38c6c8324658242491bafe46388
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-02 04:11:32 +0700

    Update page.tsx

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";;
 
 export default async function DashboardPage() {
@@ -17,3 +17,4 @@ export default async function DashboardPage() {
     </div>
   );
 }
+
