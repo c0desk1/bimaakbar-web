f90350e3cad0ae0ac43c0d74fe4dc51e6e0b37fa
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-02 03:57:03 +0700

    Add files via upload

---
+++ src/app/(web)/layout.tsx
@@ -0,0 +1,16 @@
+//main
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import { Container } from "@/components/Container"
+
+import "../globals.css"
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <Container size="md">{children}</Container>
+      <Footer />
+    </>
+  )
+}
