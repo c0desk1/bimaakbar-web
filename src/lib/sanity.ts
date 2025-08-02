48d679af394d955a3ab936c440d4fddc7df7db9b
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-03 03:31:46 +0700

    Create sanity.ts

---
+++ src/lib/sanity.ts
@@ -0,0 +1,16 @@
+// lib/sanity.ts
+import { createClient } from 'sanity';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = createClient({
+  projectId: 'OZnzDdo88',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2023-05-03',
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source) {
+  return builder.image(source);
+}
