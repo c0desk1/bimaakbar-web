4f92498792e12f8efb76a9b9252425bc32a28d5e
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-02 12:18:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { geistSans, geistMono } from "@/lib/fonts";
 import { siteMetadata } from "@/lib/metadata";
 import { ThemeScript } from "@/components/scripts/ThemeScript";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata = siteMetadata;
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeScript />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <SessionProvider>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
