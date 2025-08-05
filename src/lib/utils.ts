e4d698340ae83416c456a6c0dec465de0a767287
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-05 23:04:05 +0700

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: string | Date): string {
-  return new Intl.DateTimeFormat("id-ID", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-  }).format(new Date(date));
-}
