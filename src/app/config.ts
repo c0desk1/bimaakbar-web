45117a0ebf5e59adad98e87407db556c46a60959
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-05 23:12:53 +0700

    Delete src directory

--- src/app/config.ts
+++
@@ -1,6 +0,0 @@
-export const siteConfig = {
-    name: "Bima Akbar",
-    description: "Blog ringan dan cepat dengan Next.js",
-    url: "https://bimaakbar.vercel.app",
-    twitter: "@bimaakbarmusicc",
-}  
