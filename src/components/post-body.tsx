95fc8dd718f57cb8466992ec6d1fbf2a71f715b7
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-06 00:33:02 +0700

    Create post-body.tsx

---
+++ src/components/post-body.tsx
@@ -0,0 +1,14 @@
+// src/app/blog/[slug]/components/PostBody.tsx
+
+interface PostBodyProps {
+  content: string;
+}
+
+export default function PostBody({ content }: PostBodyProps) {
+  return (
+    <article
+      className="prose px-4"
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  );
+}
