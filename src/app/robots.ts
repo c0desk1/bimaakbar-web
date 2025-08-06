61169dd8b22a881bdc77d4f409402dac41fe00bd
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-06 17:48:17 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/admin/',
     },
-    sitemap: 'https://bimaakbar.vercel.app/sitemap.xml',
+    sitemap: 'https://bimaakbar.my.id/sitemap.xml',
   }
 }
