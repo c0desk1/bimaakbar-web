f8ea13c70af4d63c14f4d2277976b407b376e95a
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-06 15:18:54 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import { getPageBySlug } from "@/lib/posts"
 
 export default function AboutPage() {
-  const { content, data } = getPageBySlug("about")
+  const { content, data } = await getPageBySlug("about")
     return (
       <main className="relative border-x border-[var(--border)] mt-14 px-4">
       <div className="absolute left-1/2 top-0 h-full border-l border-dashed border-[var(--border)] transform z-0 opacity-20" />
@@ -11,4 +11,5 @@ export default function AboutPage() {
         </article>
       </main>
     )
+
   }  
