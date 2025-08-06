7255581a3ee599a11bf229dded1458670f8a1789
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-07 02:44:59 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import "./globals.css";
 import { geistSans, geistMono } from "@/lib/fonts";
 import { siteMetadata } from "@/lib/metadata";
 import { ThemeScript } from "@/components/scripts/ThemeScript";
-import { Analytics } from '@vercel/analytics/next';
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import { Container } from "@/components/Container"
+import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = siteMetadata;
 
@@ -16,14 +18,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <ThemeScript />
       </head>
+      <GoogleTagManager gtmId="GTM-TJXJBGLX" />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      <Analytics />
-      <Header />
-        <main>
-          <Container size="lg">{children}</Container>
-        </main>
-      <Footer />
+        <Analytics />
+        <Header />
+        <Container size="lg">{children}</Container>
+        <Footer />
       </body>
+      <GoogleAnalytics gaId="G-ZHKJ22MFVE" />
     </html>
   );
 }
