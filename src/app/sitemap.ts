d9e2a823a853a40906368082cbfad6c081d9b86d
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-06 17:44:06 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://bimaakbar.vercel.app',
+      url: 'https://bimaakbar.my.id',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://bimaakbar.vercel.app/about',
+      url: 'https://bimaakbar.my.id/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://bimaakbar.vercel.app/blog',
+      url: 'https://bimaakbar.my.id/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
