5d24c79403ef256120b9732fe78eba3c3e2552dd
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-06 19:26:24 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,10 @@
 import type { NextConfig } from "next";
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' https://www.googletagmanager.com;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  font-src 'self';
-  connect-src *;
-`;
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-];
-
-const withMDX = require("@next/mdx")();
-
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Berlaku untuk semua route
-        headers: securityHeaders,
-      },
-    ];
+  images: {
+    formats: ['image/webp', 'image/avif'],
   },
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = (nextConfig);
