17e566d204eb8b955ae010c35d1bd605711be232
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-08 01:34:52 +0700

    Update page.tsx

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function PrivacyPage() {
     return (
-        <main className="relative border-x border-[var(--border)] mt-14 px-4">
+        <main className="relative mt-14 px-4">
             <div className="absolute left-1/2 top-0 h-full border-l border-dashed border-[var(--border)] transform z-0 opacity-20" />
             <article className="prose">
                 <h1>Privasi & Kebijakan</h1>
