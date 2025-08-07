ac140c2d6f271adcc22288066f2adba83a3f0c52
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-08 01:20:53 +0700

    Delete src directory

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const siteConfig = {
-    name: "Bima Akbar",
-    description: "Selamat datang, dan sampai nanti.",
-    url: "https://bimaakbar.my.id",
-    twitter: "@bimaakbarmusicc",
-}  
