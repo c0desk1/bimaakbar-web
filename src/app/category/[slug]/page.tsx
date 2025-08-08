068c7c5043d1ccb92dfbf10898cbd44d211ce593
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-08 20:52:27 +0700

    Update page.tsx

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next"
 import Hero from "@/components/ui/Hero"
 import CategoryPostList from "@/components/CategoryPostList"
 import { capitalizeFirstLetter } from "@/lib/utils"
+import { siteConfig } from "@/config"
 
 type Params = {
   params: Promise<{ slug: string }>
@@ -15,6 +16,21 @@ export async function generateMetadata({ params }: Params): Promise<Metadata> {
   return {
     title: `${capitalizeFirstLetter(slug)}`,
     description: `Artikel dalam kategori ${capitalizeFirstLetter(slug)}.`,
+    alternates: {
+      canonical: `${siteConfig.url}/tags/${(slug)}`,
+    },
+    openGraph: {
+      title: `${capitalizeFirstLetter(slug)}`,
+      description: `Artikel dalam kategori ${capitalizeFirstLetter(slug)}.`,
+      url: `${siteConfig.url}/tags/${(slug)}`,
+      siteName: siteConfig.name,
+      locale: "id_ID",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${capitalizeFirstLetter(slug)}`,
+      description: `Artikel dalam kategori ${capitalizeFirstLetter(slug)}.`,
+    },
   }
 }
 
